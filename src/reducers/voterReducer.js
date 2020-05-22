4fa5d66e9f502ff01bdcbf639494112c9da60df0
Sudha Kaushik <Sudha_Kaushik@intuit.com>
2020-05-21 19:54:50 -0700

    Workflow1 Edit

--- src/reducers/voterReducer.js
+++ src/reducers/voterReducer.js
@@ -1,13 +1,32 @@
+import { combineReducers } from "redux";
+
 import {
     REFRESH_VOTERS_DONE_ACTION,
+    EDIT_VOTER_REQUEST_ACTION,
+    CANCEL_VOTER_REQUEST_ACTION
   } from '../actions/voterActions';
   
-  export const voterReducer = (voters = [], action) => {
-  
-    if (action.type === REFRESH_VOTERS_DONE_ACTION) {
-      return action.voters;
-    }
-    return voters;
-  
-  };
-  
+export const voterReducer = (voters = [], action) => {
+  if (action.type === REFRESH_VOTERS_DONE_ACTION) {
+    return action.voters;
+  }
+  return voters;
+};
+
+export const editVoterIdReducer = (editVoterId = -1, action) => {
+  if (action.type === EDIT_VOTER_REQUEST_ACTION) {
+    return action.voterId;
+  }
+  if (
+    action.type === REFRESH_VOTERS_DONE_ACTION ||
+    action.type === CANCEL_VOTER_REQUEST_ACTION
+  ) {
+    return -1;
+  }
+  return editVoterId;
+};
+
+export const appReducer = combineReducers({
+  voters: voterReducer,
+  editVoterId: editVoterIdReducer,
+});
