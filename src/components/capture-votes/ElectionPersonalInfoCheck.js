185e475974179bb77d46934c1f50aaf26f416e78
Sudha Kaushik <Sudha_Kaushik@intuit.com>
2020-05-21 22:34:12 -0700

    clean

--- src/components/capture-votes/ElectionPersonalInfoCheck.js
+++ src/components/capture-votes/ElectionPersonalInfoCheck.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from "react";
 import { useForm } from "../../hooks/useForm";
 import { useHistory } from "react-router-dom";
--- src/components/pages/ElectionDataPage.js
+++ src/components/pages/ElectionDataPage.js
@@ -1,5 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
 
 import { SectionHeader } from '../blocks/SectionHeader';
 import { ContentSection } from '../blocks/ContentSection';
@@ -10,19 +10,12 @@ export const ElectionDataPage = ({
     onSubmitElectionData: addElection, elections, onRefreshElectionData: refreshElection
 }) => {
 
-  const history = useHistory();
-
   useEffect(() => {
     refreshElection();
   }, []);
   
   const addElectionAndRedirect = (electiondata) => {
     addElection(electiondata);
-    /*
-    addElection(electiondata).then(() => {
-      history.push('/');
-    })
-    */
   }
 
   return (
